801f33d18c4f2c81dc51fb3837ba31de7357ae64
soojeong <tnwjd7890@gmail.com>
2023-04-13 14:50:36 +0900

    component, pages 폴더 생성

---
+++ src/components/TodoItem/index.jsx
@@ -0,0 +1,3 @@
+const TodoItem = () => {};
+
+export default TodoItem;
---
+++ src/pages/Main/index.jsx
@@ -0,0 +1,10 @@
+import Input from "../../components/Input";
+import TodoItem from "../../components/TodoItem";
+
+const Main = () => {
+    <Input />
+    <TodoItem />
+
+};
+
+export default Main;
